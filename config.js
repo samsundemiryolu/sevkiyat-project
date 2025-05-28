737f50c80605b4d57bb486e26a0a08b30ad14fad
KAMİL ASAN <kamilasan@samsunlojistik.com.tr>
2025-05-28 16:10:06 +0300

    PORT tekrar tanımı düzeltildi

--- config.js
+++ config.js
@@ -1,18 +1,23 @@
 const path = require('path');
+const isLocal = process.env.NODE_ENV !== 'production';
 
-const dataDirectory = path.join(__dirname, "data"); // HER ZAMAN bu olacak
+// Ortak JSON dizini tanımı
+const dataDirectory = isLocal
+  ? path.join(__dirname, "data")           // Electron için
+  : "/app/data";                           // Railway için
 
 module.exports = {
-  baseUrl: process.env.NODE_ENV !== "production"
+  baseUrl: isLocal
     ? "http://localhost:3000"
     : "https://sevkiyat-project.up.railway.app",
   dataPath: dataDirectory,
   paths: {
-    ships:     path.join(dataDirectory, "ships.json"),
-    firms:     path.join(dataDirectory, "firms.json"),
-    checklists:path.join(dataDirectory, "checklists.json"),
-    shipment:  path.join(dataDirectory, "shipment.json"),
-    vehicles:  path.join(dataDirectory, "vehicles.json"),
-    erbosan:   path.join(dataDirectory, "erbosan.json"),
+    ships: path.join(dataDirectory, "ships.json"),
+    firms: path.join(dataDirectory, "firms.json"),
+    checklists: path.join(dataDirectory, "checklists.json"),
+    shipment: path.join(dataDirectory, "shipment.json"),
+    vehicles: path.join(dataDirectory, "vehicles.json"),
+    erbosan: path.join(dataDirectory, "erbosan.json"),
+    odeme: path.join(dataDirectory, "odeme.json")
   }
 };
